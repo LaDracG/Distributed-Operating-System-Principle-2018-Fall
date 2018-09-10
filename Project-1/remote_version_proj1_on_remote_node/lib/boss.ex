8316e9a22877d8a948e13b468174872901d352c9
wyfunique <yifanwang2017@gmail.com>
2018-09-10 17:56:45 -0400

    On branch dev  Changes to be committed: 	Added the remote version code which is deployed on remote nodes, packed in folder "remote_version_proj1_on_remote_node" 	new file:   remote_version_proj1_on_remote_node/.formatter.exs 	new file:   remote_version_proj1_on_remote_node/.gitignore 	new file:   remote_version_proj1_on_remote_node/README.md 	new file:   remote_version_proj1_on_remote_node/config/config.exs 	new file:   remote_version_proj1_on_remote_node/lib/boss.ex 	new file:   remote_version_proj1_on_remote_node/lib/daemon.ex 	new file:   remote_version_proj1_on_remote_node/lib/worker.ex 	new file:   remote_version_proj1_on_remote_node/mix.exs 	new file:   remote_version_proj1_on_remote_node/proj1.exs 	new file:   remote_version_proj1_on_remote_node/remote_node.log 	new file:   remote_version_proj1_on_remote_node/test/remote_version_proj1_test.exs 	new file:   remote_version_proj1_on_remote_node/test/test_helper.exs

---
+++ Project-1/remote_version_proj1_on_remote_node/lib/boss.ex
@@ -0,0 +1,97 @@
+defmodule Boss do
+	# Do not try to use module attributes to record intermediate states
+	# Module attributes are immutable.
+	# So the better way is passing the states as parameters into the next function call. 
+	@name :boss
+	@work_unit 50
+	
+	def scheduler(n, k, worker_list, ans, start) do
+		receive do
+			{worker_pid, :fetch_task, list} ->
+				worker_list = 
+					if list != [] do
+						worker_list -- [worker_pid]
+					else
+						# We must assign var to itself if no change happened.
+						# If we do not care about this case, the var will be nil instead of keeping original value in this case.
+						worker_list 
+					end
+				
+				ans = 
+					if list == [nil] do
+						ans
+					else
+						ans ++ list
+					end	
+				
+				e = 
+					if start <= n do
+						if start + @work_unit > n do
+							n
+						else
+							start + @work_unit - 1
+						end
+					end
+
+				if start <= n do
+					send(worker_pid, {:ok, start, e, k})
+				else
+					send(worker_pid, {:no_task})
+				end
+
+				worker_list = 
+					if start <= n do
+						worker_list ++ [worker_pid]
+					else
+						worker_list
+					end
+					
+				start = 
+					if start <= n do
+						start + @work_unit
+					else
+						start
+					end
+				#IO.puts inspect(worker_list)
+				#IO.puts start
+				#IO.puts n
+				#IO.puts inspect(ans)
+				#IO.puts "Test"
+
+				if start > n and worker_list == [] do
+					#IO.puts "Task Done. Results: "
+					ans = Enum.sort(ans)
+					for num <- ans do
+						IO.puts num
+					end
+					stop()
+				else
+					#IO.puts "Continue"
+					scheduler(n, k, worker_list, ans, start)
+				end
+
+			{_, :print_ans} ->
+				for num <- ans do
+					IO.puts num
+				end
+		end
+		
+	end
+
+	def pid, do: Process.whereis(@name)
+
+	def printAns do
+		send(self(), :print_ans)
+	end
+
+	def start(n, k, worker_list, ans, start) do
+		pid = spawn(__MODULE__, :scheduler, [n, k, worker_list, ans, start])
+		Process.register(pid, @name)
+	end
+
+	def stop do
+		Process.exit(pid(), :normal)
+		Process.unregister(@name)
+	end
+
+end
