c55891e2d99a74020fb7af1a0591d7601a8c074c
wyfunique <yifanwang2017@gmail.com>
2018-11-03 18:22:01 -0400

    On branch dev  Changes to be committed:     Added project 4.1

---
+++ project_4_1/.elixir_ls/build/test/lib/project_4_1/ebin/project_4_1.app
@@ -0,0 +1,6 @@
+{application,project_4_1,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"project_4_1"},
+              {modules,['Elixir.Project3.CLI']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ project_4_1/lib/project41_cli.ex
@@ -0,0 +1,29 @@
+defmodule Project3.CLI do
+  def main(args \\ []) do
+    {opts, words, _} =
+      OptionParser.parse(args, switches: [])
+    #IO.puts inspect(word)
+    [num_nodes] = words
+    {num_nodes, _} = Integer.parse(num_nodes)
+    #{num_reqs, _} = Integer.parse(num_reqs)
+    #IO.puts inspect(num_nodes) <> " " <> inspect(num_reqs)
+    if num_nodes < 3 do
+      IO.puts "The number of peers cannot be less than 3!"
+    else
+      #Manager.start(num_nodes)
+      loop()
+    end
+  end
+
+  def loop() do
+    loop()
+  end
+
+  """
+  def waitNetworkFinish(net_pid) do
+    if Process.alive?(net_pid) do
+      waitNetworkFinish(net_pid)
+    end
+  end
+  """
+end
